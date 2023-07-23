ccfaa45adce4919b10e3c7752c912dfc7b076e5d
Vinnie <zvinayak83@gmail.com>
2023-07-23 00:42:12 -0400

    initial setup

--- src/main/java/net/vixellpedia/boxmod/boxmod.java
+++ src/main/java/net/vixellpedia/boxmod/boxmod.java
@@ -1,4 +1,4 @@
-package net.kaupenjoe.tutorialmod;
+package net.vixellpedia.boxmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,13 +11,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod {
-    public static final String MOD_ID = "tutorialmod";
+@Mod(boxmod.MOD_ID)
+public class boxmod {
+    public static final String MOD_ID = "boxmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     // Very Important Comment
-    public TutorialMod() {
+    public boxmod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
