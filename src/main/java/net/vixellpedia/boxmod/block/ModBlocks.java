95e1c32404f1d0edf6077e41e605f8028780d77a
Vinnie <zvinayak83@gmail.com>
2023-07-23 02:46:26 -0400

    custom block tutorial done

--- src/main/java/net/vixellpedia/boxmod/block/ModBlocks.java
+++ src/main/java/net/vixellpedia/boxmod/block/ModBlocks.java
@@ -1,20 +1,20 @@
 package net.vixellpedia.boxmod.block;
 
+import net.vixellpedia.boxmod.boxmod;
+import net.vixellpedia.boxmod.item.ModCreativeModeTab;
+import net.vixellpedia.boxmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.RegistryObject;
-import net.vixellpedia.boxmod.boxmod;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.vixellpedia.boxmod.item.ModCreativeModeTab;
-import net.vixellpedia.boxmod.item.ModItems;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
@@ -37,7 +37,7 @@ public class ModBlocks {
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name,block);
-
+        registerBlockItem(name,toReturn,tab);
         return toReturn;
     }
 
